f9f33cb6a298fceb03cbae4d707de9724d2e596e
Vlad Tepesch <1601159+vladtepesch@users.noreply.github.com>
2025-06-20 00:59:50 +0200

    add symbol description defines

---
+++ include/fontSymbols.h
@@ -0,0 +1,173 @@
+
+#ifndef PIXELFONTS_SMBOLS_H
+#define PIXELFONTS_SMBOLS_H
+
+// most fonts included resemble code page 850 (aka  DOS Latin 1)
+
+#define FONT_SYMBOL_SMILEY                      '\x01'  // ☺︎
+#define FONT_SYMBOL_SMILEY_INV                  '\x02'  // ☻
+#define FONT_SYMBOL_HEART                       '\x03'  // ♥︎
+#define FONT_SYMBOL_DIAMONDS                    '\x04'  // ♦︎
+#define FONT_SYMBOL_CLUBS                       '\x06'  // ♣︎
+#define FONT_SYMBOL_SPADES                      '\x07'  // ♠︎
+#define FONT_SYMBOL_MALE                        '\x0B'  // ♂︎
+#define FONT_SYMBOL_FEMALE                      '\x0C'  // ♀︎
+#define FONT_SYMBOL_NOTE                        '\x0E'  // ♫
+#define FONT_SYMBOL_SUN                         '\x0F'  // ☼
+
+#define FONT_SYMBOL_TRIANGLE_LEFT               '\x10'  // ►
+#define FONT_SYMBOL_TRIANGLE_RIGHT              '\x11'  // ◄
+#define FONT_SYMBOL_ARROW_UP_DOWN               '\x12'  // ↕︎
+#define FONT_SYMBOL_DOULBE_EXCLAMATION          '\x13'  // ‼︎
+#define FONT_SYMBOL_PARAGRAPH_TYPOGRAPHIC       '\x14'  // ¶
+#define FONT_SYMBOL_PARAGRAPH_LEGAL             '\x15'  // §
+#define FONT_SYMBOL_LINE_BLOCK                  '\x16'  // ▬
+#define FONT_SYMBOL_ARROW_UP_DOWN_UNDERLINE     '\x17'  // ↨
+#define FONT_SYMBOL_ARROW_UP                    '\x18'  // ↑
+#define FONT_SYMBOL_ARROW_DOWN                  '\x19'  // ↓
+#define FONT_SYMBOL_ARROW_ARROR_RIGHT           '\x1A'  // →
+#define FONT_SYMBOL_ARROW_ARROR_LEFT            '\x1B'  // ←
+#define FONT_SYMBOL_RIGHT_ANGLE_BL              '\x1C'  // ∟
+#define FONT_SYMBOL_ARROW_LEFT_RIGHT            '\x1D'  // ↔︎
+#define FONT_SYMBOL_TRIANGLE_UP                 '\x1E'  // ▲
+#define FONT_SYMBOL_TRIANGLE_DOWN               '\x1F'  // ▼
+// 0x20 - 0x7E (32-126) are normal ASCII   
+#define FONT_SYMBOL_RECTANGLE                   '\x7F'  // ▯
+
+#define FONT_SYMBOL_LARGE_C_CEDILLA             '\x80'  // Ç
+#define FONT_SYMBOL_SMALL_U_UMLAUT              '\x81'  // ü
+#define FONT_SYMBOL_SMALL_E_ACUTE               '\x82'  // é
+#define FONT_SYMBOL_SMALL_A_CIRCUMFLEX          '\x83'  // â
+#define FONT_SYMBOL_SMALL_A_UMLAUT              '\x84'  // ä
+#define FONT_SYMBOL_SMALL_A_GRAVE               '\x85'  // à
+#define FONT_SYMBOL_SMALL_A_OVERRING            '\x86'  // å
+#define FONT_SYMBOL_SMALL_C_CEDILLA             '\x87'  // ç
+#define FONT_SYMBOL_SMALL_E_CIRCUMFLEX          '\x88'  // ê
+#define FONT_SYMBOL_SMALL_E_UMLAUT              '\x89'  // ë
+#define FONT_SYMBOL_SMALL_E_GRAVE               '\x8A'  // è
+#define FONT_SYMBOL_SMALL_I_UMLAUT              '\x8B'  // ï
+#define FONT_SYMBOL_SMALL_I_CIRCUMFLEX          '\x8C'  // î
+#define FONT_SYMBOL_SMALL_I_GRAVE               '\x8D'  // ì
+#define FONT_SYMBOL_LARGE_A_UMLAUT              '\x8E'  // Ä
+#define FONT_SYMBOL_LARGE_A_OVERRING            '\x8F'  // Å
+
+#define FONT_SYMBOL_LARGE_E_ACUTE               '\x90'  // É
+#define FONT_SYMBOL_SMALL_AE                    '\x91'  // æ
+#define FONT_SYMBOL_LARGE_AE                    '\x92'  //	Æ
+#define FONT_SYMBOL_SMALL_O_CIRCUMFLEX          '\x93'  // ô
+#define FONT_SYMBOL_SMALL_O_UMLAUT              '\x94'  // ö
+#define FONT_SYMBOL_SMALL_O_GRAVE               '\x95'  // ò
+#define FONT_SYMBOL_SMALL_U_CIRCUMFLEX          '\x96'  // û
+#define FONT_SYMBOL_SMALL_U_GRAVE               '\x97'  // ù
+#define FONT_SYMBOL_SMALL_Y_UMLAUT              '\x98'  // ÿ
+#define FONT_SYMBOL_LARGE_O_UMLAUT              '\x99'  // O
+#define FONT_SYMBOL_LARGE_U_UMLAUT              '\x9A'  // Ü
+#define FONT_SYMBOL_SMALL_O_CROSSED             '\x9B'  // ø
+#define FONT_SYMBOL_CURRENCY_POUND              '\x9C'  // £
+#define FONT_SYMBOL_LARGE_O_CROSSED             '\x9D'  // Ø
+#define FONT_SYMBOL_CROSS_DIAG                  '\x9E'  // ×
+#define FONT_SYMBOL_SMALL_F_HOOKED              '\x9F'  // ƒ
+
+#define FONT_SYMBOL_SMALL_A_ACUTE               '\xA0'  // á
+#define FONT_SYMBOL_SMALL_I_ACUTE               '\xA1'  // í
+#define FONT_SYMBOL_SMALL_O_ACUTE               '\xA2'  // ó
+#define FONT_SYMBOL_SMALL_U_ACUTE               '\xA3'  // ú
+#define FONT_SYMBOL_SMALL_N_TILDE               '\xA4'  // ñ
+#define FONT_SYMBOL_LARGE_N_TILDE               '\xA5'  // Ñ
+#define FONT_SYMBOL_SMALL_SUPERSCRIPT_A         '\xA6'  // ª
+#define FONT_SYMBOL_SMALL_SUPERSCRIPT_O         '\xA7'  // º
+#define FONT_SYMBOL_QUESTIONMARK_UPSIDE_DOWN    '\xA8'  // ¿
+#define FONT_SYMBOL_REGISTERED_TRADE_MARK       '\xA9'  // ®
+#define FONT_SYMBOL_LOGIC_NOT                   '\xAA'  // ¬
+#define FONT_SYMBOL_HALF                        '\xAB'  // ½
+#define FONT_SYMBOL_QUARTER                     '\xAC'  // ¼
+#define FONT_SYMBOL_EXCLAMATIONMARK_UPSIDE_DOWN '\xAD'  // ¡
+#define FONT_SYMBOL_DOUBLE_CHEVRON_LEFT         '\xAE'  // «
+#define FONT_SYMBOL_DOUBLE_CHEVRON_RIGHT        '\xAF'  // »
+
+#define FONT_SYMBOL_SHADE_LIGH                  '\xB0'  // ░
+#define FONT_SYMBOL_SHADE_MEDIUM                '\xB1'  // ▒
+#define FONT_SYMBOL_SHADE_HARD                  '\xB2'  // ▓
+#define FONT_SYMBOL_BOX_LINE_V                  '\xB3'  // │
+#define FONT_SYMBOL_BOX_LINE_V_SPLIT_L          '\xB4'  // ┤
+#define FONT_SYMBOL_LARGE_A_ACUTE               '\xB5'  // Á
+#define FONT_SYMBOL_LARGE_A_CIRCUMFLEX          '\xB6'  // Â
+#define FONT_SYMBOL_LARGE_A_GRAVE               '\xB7'  // À
+#define FONT_SYMBOL_COPYRIGHT_TRADE_MARK        '\xB8'  // ©
+#define FONT_SYMBOL_BOX_DLINE_V_DSPLIT_L        '\xB9'  // ╣
+#define FONT_SYMBOL_BOX_DLINE_V                 '\xBA'  // ║
+#define FONT_SYMBOL_BOX_DCORNER_TR              '\xBB'  // ╗
+#define FONT_SYMBOL_BOX_DCORNER_BR              '\xBC'  // ╝
+#define FONT_SYMBOL_CURRENCY_CENT               '\xBD'  // ¢
+#define FONT_SYMBOL_CURRENCY_YEN                '\xBE'  // ¥
+#define FONT_SYMBOL_BOX_CORNER_TR               '\xBF'  // ┐
+
+#define FONT_SYMBOL_BOX_CORNER_BL               '\xC0'  // └
+#define FONT_SYMBOL_BOX_LINE_H_SPLIT_T          '\xC1'  // ┴
+#define FONT_SYMBOL_BOX_LINE_H_SPLIT_B          '\xC2'  // ┬
+#define FONT_SYMBOL_BOX_LINE_V_SPLIT_R          '\xC3'  // ├
+#define FONT_SYMBOL_BOX_LINE_H                  '\xC4'  // ─
+#define FONT_SYMBOL_BOX_LINE_CROSS              '\xC5'  // ┼
+#define FONT_SYMBOL_SMALL_A_TILDE               '\xC6'  // ã
+#define FONT_SYMBOL_LARGE_A_TILDE               '\xC7'  // Ã
+#define FONT_SYMBOL_BOX_DCORNER_BL              '\xC8'  // ╚
+#define FONT_SYMBOL_BOX_DCORNER_TL              '\xC9'  // ╔
+#define FONT_SYMBOL_BOX_DLINE_H_DSPLIT_T        '\xCA'  // ╩
+#define FONT_SYMBOL_BOX_DLINE_H_DSPLIT_B        '\xCB'  // ╦
+#define FONT_SYMBOL_BOX_DLINE_V_DSPLIT_R        '\xCC'  // ╠
+#define FONT_SYMBOL_BOX_DLINE_H                 '\xCD'  // ═
+#define FONT_SYMBOL_BOX_DLINE_CROSS             '\xCE'  // ╬
+#define FONT_SYMBOL_CURRENCY_SIGN               '\xCF'  // ¤
+
+#define FONT_SYMBOL_SMALL_ETH                   '\xD0'  // ð
+#define FONT_SYMBOL_LARGE_ETH                   '\xD1'  // Ð
+#define FONT_SYMBOL_LARGE_E_CIRCUMFLEX          '\xD2'  // Ê
+#define FONT_SYMBOL_LARGE_E_UMLAUT              '\xD3'  // Ë
+#define FONT_SYMBOL_LARGE_E_GRAVE               '\xD4'  // È
+#define FONT_SYMBOL_SMALL_I_WITHOUT_DOT         '\xD5'  // ı
+#define FONT_SYMBOL_LARGE_I_ACUTE               '\xD6'  // Í
+#define FONT_SYMBOL_LARGE_I_CIRCUMFLEX          '\xD7'  // Î
+#define FONT_SYMBOL_LARGE_I_UMLAUT              '\xD8'  // Ï
+#define FONT_SYMBOL_BOX_CORNER_BR               '\xD9'  // ┘
+#define FONT_SYMBOL_BOX_CORNER_TL               '\xDA'  // ┌
+#define FONT_SYMBOL_BLOCK_FULL                  '\xDB'  // █
+#define FONT_SYMBOL_BLOCK_LOWER_HALF            '\xDC'  // ▄
+#define FONT_SYMBOL_BAR_V_BROKEN                '\xDD'  // ¦
+#define FONT_SYMBOL_LARGE_I_GRAVE               '\xDE'  // Ì
+#define FONT_SYMBOL_BLOCK_UPPER_HALF            '\xDF'  // ▀
+
+#define FONT_SYMBOL_LARGE_O_ACUTE               '\xE0'  // Ó
+#define FONT_SYMBOL_SMALL_SZET                  '\xE1'  // ß
+#define FONT_SYMBOL_LARGE_O_CIRCUMFLEX          '\xE2'  // Ô
+#define FONT_SYMBOL_LARGE_O_GRAVE               '\xE3'  // Ò
+#define FONT_SYMBOL_SMALL_O_TILDE               '\xE4'  // õ
+#define FONT_SYMBOL_LARGE_O_TILDE               '\xE5'  // Õ
+#define FONT_SYMBOL_SMALL_MY                    '\xE6'  // µ
+#define FONT_SYMBOL_SMALL_THORN                 '\xE7'  // þ
+#define FONT_SYMBOL_LARGE_THORN                 '\xE8'  // Þ
+#define FONT_SYMBOL_LARGE_U_ACUTE               '\xE9'  // Ú
+#define FONT_SYMBOL_LARGE_U_CIRCUMFLEX          '\xEA'  // Û
+#define FONT_SYMBOL_LARGE_U_GRAVE               '\xEB'  // Ù
+#define FONT_SYMBOL_SMALL_Y_ACUTE               '\xEC'  // ý
+#define FONT_SYMBOL_LARGE_Y_GRAVE               '\xED'  // Ý
+#define FONT_SYMBOL_OVERLINE                    '\xEE'  // ¯
+#define FONT_SYMBOL_BACKTICK                    '\xEF'  // ´
+
+#define FONT_SYMBOL_HYTHON                      '\xF0'  // -
+#define FONT_SYMBOL_PLUS_MINUS                  '\xF1'  // ±
+#define FONT_SYMBOL_DOUBLE_UNDERLINE            '\xF2'  // ‗
+#define FONT_SYMBOL_THREE_QUARTER               '\xF3'  // ¾
+#define FONT_SYMBOL_PARAGRAPH_TYPOGRAPHIC_2     '\xF4'  // ¶
+#define FONT_SYMBOL_PARAGRAPH_LEGAL_2           '\xF5'  // §
+#define FONT_SYMBOL_DIVIDE_SYMBOL               '\xF6'  // ÷
+#define FONT_SYMBOL_CEDILLA                     '\xF7'  // ¸
+#define FONT_SYMBOL_OVERRING                    '\xF8'  // °
+#define FONT_SYMBOL_UMLAUT_DOTS                 '\xF9'  // ¨
+#define FONT_SYMBOL_CDOT                        '\xFA'  // ·
+#define FONT_SYMBOL_SMALL_SUPERSCRIPT_1         '\xFB'  // ¹
+#define FONT_SYMBOL_SMALL_SUPERSCRIPT_3         '\xFC'  // ³
+#define FONT_SYMBOL_SMALL_SUPERSCRIPT_2         '\xFD'  // ²
+#define FONT_SYMBOL_SQUARE_FILLED               '\xFE'  // ■
+#define FONT_SYMBOL_NBSP                        '\xFF'  // NBSP
+
+#endif // PIXELFONTS_SMBOLS_H
